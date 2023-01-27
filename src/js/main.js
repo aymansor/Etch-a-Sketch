cfd6158dcb3cd825bafc4a21a4d19f08f74ba7be
aymansor <almansorayman@gmail.com>
2023-01-27 09:25:56 -0500

    feat: Add mouseover drawing functionality to grid squares
    - Add mousedown and mouseover event listeners to each grid square
    - On mousedown, change the square's background color to black
    - On mouseover, check if left mouse button is held down and change
    the square's background color to black
    - Add a check to confirm that event is triggered by left mouse button
    - Add a check to reset square color if right mouse button is clicked

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 let grid = document.getElementById("grid");
 
 let gridSize = 16;
-let gridColor = window
+let gridBackgroundColor = window
   .getComputedStyle(document.documentElement)
   .getPropertyValue("--clr-grid");
 
@@ -11,10 +11,39 @@ const CreateGrid = () => {
   for (let i = 0; i < gridSize * gridSize; i++) {
     let square = document.createElement("div");
     square.classList.add("grid-square");
-    square.style.backgroundColor = "#ffffff";
+    square.style.backgroundColor = gridBackgroundColor;
     square.style.width = `${100 / gridSize}%`;
+
+    // Add mousedown event listener to each square
+    square.addEventListener("mousedown", (event) => {
+      gridAction(event);
+    });
+
+    // Add mouseover event listener to each square
+    square.addEventListener("mouseover", (event) => {
+      gridAction(event);
+    });
+
     grid.appendChild(square);
   }
 };
 
-CreateGrid();
+let gridAction = (event) => {
+  // check if left mouse button is pressed
+  if (event.buttons === 1) {
+    event.target.style.backgroundColor = "black";
+    // check if left mouse button is held down
+  } else if (event.buttons === 2) {
+    event.target.style.backgroundColor = gridBackgroundColor;
+  }
+};
+
+let main = () => {
+  // disable right click inside the grid
+  grid.addEventListener("contextmenu", (e) => e.preventDefault());
+
+  // create the grid
+  CreateGrid();
+};
+
+main();
