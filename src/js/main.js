499ac420f30164997dcb0e09806b1951542261a8
aymansor <almansorayman@gmail.com>
2023-01-27 09:10:02 -0500

    Implement a dynamic grid of square divs
    Also, added new css properties for the grid element
    and grid-square class"

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let grid = document.getElementById("grid");
+
+let gridSize = 16;
+let gridColor = window
+  .getComputedStyle(document.documentElement)
+  .getPropertyValue("--clr-grid");
+
+const CreateGrid = () => {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let square = document.createElement("div");
+    square.classList.add("grid-square");
+    square.style.backgroundColor = "#ffffff";
+    square.style.width = `${100 / gridSize}%`;
+    grid.appendChild(square);
+  }
+};
+
+CreateGrid();
